64b778efc02523a9caa2f6943d61ee0355921933
Lucas P <lucas.panta@gmail.com>
2016-08-18 16:29:26 +0800

    add fixes on access rights for Symfony 3; add info, warn and errors messages

--- symfony-create.sh
+++ symfony-create.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -e
+
+ERR="\033[0;31m"
+WAR="\033[0;33m"
+INF="\033[0;36m"
+SUC="\033[0;32m"
+RES="\033[0m"
 
 APP=$1
 VERSION=$2
@@ -7,66 +12,101 @@ VERSION=$2
 ROOT="/var/www/$APP"
 SERVER_BLOCK="/etc/nginx/sites-available/$APP"
 
-if [[  -z "$APP" || "$APP" =~ [^a-zA-Z0-9-] ]]; then
-  echo "Invalid project name. Only alphanumerics and hyphens characters are allowed."
+if [[  -z $APP || $APP =~ [^a-z0-9-] ]]; then
+  echo -e $ERR"Invalid project name. Only numbers, hyphens and lowercase characters are allowed."$RES
   exit 1
 else 
-  if [ -d "$ROOT" ]; then
-    echo "The project $APP already exist."
+  if [ -d $ROOT ]; then
+    echo -e $ERR"The project $APP already exist. Please choose another name."$RES
     exit 1
   fi
 fi
 
 # Create Symfony app
-cd /var/www
-symfony new $APP $VERSION
+echo -e $INF"Creating Symfony project..."$RES
+{
+  cd /var/www
+  symfony new $APP $VERSION
+} || {
+  echo -e $ERR"Error while creating Symfony project."$RES
+  exit 1
+}
 
-# Update dependencies 
-cd $APP
-composer update
+# Update dependencies
+echo -e $INF"Updating project dependencies..."$RES
+{
+  cd $APP
+  composer update
+} || {
+  echo -e $WAR"Error while updating dependencies."$RES
+}
 
 # Fix access issues
-apt-get update && apt-get install -y acl
-HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-mkdir -p $ROOT/app/cache $ROOT/app/logs
-(
-setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX $ROOT/app/cache $ROOT/app/logs
-setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX $ROOT/app/cache $ROOT/app/logs
-) || true
+echo -e $INF"Setting up access rights..."$RES
+{
+  apt-get update && apt-get install -y acl
+  HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+  if [[ $VERSION == 2* ]]; then
+    # Symfony 2.x
+    mkdir -p $ROOT/app/cache $ROOT/app/logs
+    setfacl -R -m u:$HTTPDUSER:rwX -m u:`whoami`:rwX $ROOT/app/cache $ROOT/app/logs
+    setfacl -dR -m u:$HTTPDUSER:rwX -m u:`whoami`:rwX $ROOT/app/cache $ROOT/app/logs
+  else  
+    # Symfony 3.x
+    mkdir -p $ROOT/var/cache $ROOT/var/logs
+    setfacl -R -m u:$HTTPDUSER:rwX -m u:`whoami`:rwX $ROOT/var/cache $ROOT/var/logs
+    setfacl -dR -m u:$HTTPDUSER:rwX -m u:`whoami`:rwX $ROOT/var/cache $ROOT/var/logs
+  fi
+  # Enable access to app_dev.php and config.php 
+  sed -i '/if (isset($_SERVER/,+6 s/^/\/\//' $ROOT/web/app_dev.php
+  sed -i '/if (!in_array(@$_SERVER/,+6 s/^/\/\//' $ROOT/web/config.php
+} || {
+  echo -e $WAR"Error encountered while setting up access rights. You may have to manually correct them."$RES
+}
 
-sed -i '/if (isset($_SERVER/,+6 s/^/\/\//' $ROOT/web/app_dev.php
-sed -i '/if (!in_array(@$_SERVER/,+6 s/^/\/\//' $ROOT/web/config.php
 
 # Create Nginx server block file
-cat > $SERVER_BLOCK <<- _EOF
-server {
-  listen 80;
-  listen [::]:80;
+echo -e $INF"Adding the website configuration to Nginx..."$RES
+{
 
-  server_name $APP.dev;
+  cat << __EOF |  sed 's/^  //' > $SERVER_BLOCK 
+  server {
+    listen 80;
+    listen [::]:80;
 
-  root $ROOT/web;
+    server_name $APP.dev;
 
-  location / {
-      try_files \$uri /app_dev.php\$is_args\$args;
+    root $ROOT/web;
+
+    location / {
+        try_files \$uri /app_dev.php\$is_args\$args;
+    }
+
+    location ~ ^/(app_dev|config)\.php(/|$) {
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
+        include fastcgi_params;
+        fastcgi_param  SCRIPT_FILENAME  \$realpath_root\$fastcgi_script_name;
+        fastcgi_param DOCUMENT_ROOT \$realpath_root;
+    }
+
+    error_log /var/log/nginx/$APP.log;
+    access_log /var/log/nginx/$APP.log;
   }
+__EOF
 
-  location ~ ^/(app_dev|config)\.php(/|$) {
-      fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
-      fastcgi_split_path_info ^(.+\.php)(/.*)$;
-      include fastcgi_params;
-      fastcgi_param  SCRIPT_FILENAME  \$realpath_root\$fastcgi_script_name;
-      fastcgi_param DOCUMENT_ROOT \$realpath_root;
-  }
+  # Enable the new server block configuration
+  ln -s $SERVER_BLOCK /etc/nginx/sites-enabled/
+  nginx -t && service nginx reload
 
-  error_log /var/log/nginx/$APP.log;
-  access_log /var/log/nginx/$APP.log;
+} || {
+
+  echo -e $ERR"Error encountered while setting up web access to the project. Your project is created but will not be accessible via its domain name."$RES
+  rm /etc/nginx/sites-enabled/$APP
+  rm $SERVER_BLOCK
+  nginx -t && service nginx reload
+  exit 1
+
 }
-_EOF
 
-# Enable the new server block configuration
-ln -s $SERVER_BLOCK /etc/nginx/sites-enabled/
-nginx -t && service nginx reload
-
-
-echo ">>>>> Your Symfony app is now available at http://$APP.dev !"
+echo -e $SUC"Your Symfony app is now available at http://$APP.dev !"$RES
